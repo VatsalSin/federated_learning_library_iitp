93679dfc55f448c36dd9e4941b40140391852db4
Vatsal Singhal <vatsalsinghal@Vatsals-MacBook-Air.local>
2020-05-04 17:58:26 +0530

    bug fix and proper file organsisation

---
+++ Eample Churn Model/client3.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import pandas as pd
+from federatedlearningiitp import *
+
+
+
+host = ""
+port = 5000
+pport = 5001
+preprocessing = requestPreprocessing(host, port)
+weights = requestWeights(host, port)
+getModel = requestModelGen(host, port)
+dataCleaner = requestDataCleaner(host,port)
+dataset = dataCleaner(pd.read_csv('Churn_Modelling.csv'))
+X = dataset.iloc[:, 3:13].values[7000:10000]
+y = dataset.iloc[:, 13].values[7000:10000]
+X = preprocessing(X)
+weights = startFederatedLearning(host, port, pport, weights, getModel, X, y,10)
+print("Training Completed Successfully")
+
+
---
+++ Eample Churn Model/client4.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import pandas as pd
+from federatedlearningiitp import *
+
+
+
+host = ""
+port = 5000
+pport = 5001
+preprocessing = requestPreprocessing(host, port)
+weights = requestWeights(host, port)
+getModel = requestModelGen(host, port)
+dataCleaner = requestDataCleaner(host,port)
+dataset = dataCleaner(pd.read_csv('Churn_Modelling.csv'))
+X = dataset.iloc[:, 3:13].values[4000:7000]
+y = dataset.iloc[:, 13].values[4000:7000]
+X = preprocessing(X)
+eights = startFederatedLearning(host, port, pport, weights, getModel, X, y,500)
+print("Training Completed Successfully")
